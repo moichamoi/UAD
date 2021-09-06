8b8ca3bc5bb5afe8744a73d742c078ec11b641d6
moichamoi <88809829+moichamoi@users.noreply.github.com>
2021-09-06 18:12:04 -0500

    Update matrices.py

--- matrices.py
+++ matrices.py
@@ -2,35 +2,35 @@ fi=int(input("¿Cuantas filas tiene la primera matriz? "))
 col1=int(input("¿Cuantas columnas tiene la primera matriz? "))
 col2=int(input("¿Cuantas columnas tiene la segunda matriz? "))
 
-A=[]
+mat1=[]
 for i in range (fi):
-    A.append([0]*col1)
+    mat1.append([0]*col1)
 
 
-B=[]
+mat2=[]
 for i in range (col1):
-    B.append([0]*col2)
+    mat2.append([0]*col2)
 
 print ("Para tu Primera matriz: ")
 for i in range (fi):
     for j in range (col1):
-        A[i][j]=float(input("Cual es el numero en la posicion (%d,%d): " % (i,j)))
+        mat1[i][j]=float(input("Cual es el numero en la posicion (%d,%d): " % (i,j)))
 
 print ("Para tu Segunda matriz: ")
 for i in range (col1):
     for j in range (col2):
-        B[i][j]=float(input("Cual es el numero en la posicion (%d,%d): " % (i,j)))
+        mat2[i][j]=float(input("Cual es el numero en la posicion (%d,%d): " % (i,j)))
 
 
-C=[]
+matf=[]
 for i in range (fi):
-    C.append([0]*col2)
+    matf.append([0]*col2)
 
 
 for i in range (fi):
     for j in range (col2):
         for k in range (col1):
-            C[i][j] += A[i][k] * B[k][j]
+            matf[i][j] += mat1[i][k] * mat2[k][j]
 
 for i in range (fi):
-    print(C[i])
+    print(matf[i])
